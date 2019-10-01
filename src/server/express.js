ce049cf18c92e88c564a3f92e98174be6db95b0f
Israel Neves Micha <isrmicha@gmail.com>
2019-10-01 17:07:51 -0300

    Update express.js

--- src/server/express.js
+++ src/server/express.js
@@ -5,6 +5,8 @@ const public = express.static('./dist')
 
 server.use(public)
 
+server.use('/oi', (req, res) => res.send('ok'))
+
 server.listen(8080, () => {
   console.log('Servidor escutando...')
 })
